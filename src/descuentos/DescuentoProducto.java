4d209a562a960eb9831b9f92d71dad77386bc3c0
NahuelAde <nahuel.ade@gmail.com>
2024-05-12 09:17:46 +0200

    Añadimos comentarios Javadoc

--- src/descuentos/DescuentoProducto.java
+++ src/descuentos/DescuentoProducto.java
@@ -1,10 +1,24 @@
 package descuentos;
 
+/**
+ * Clase DescuentoProducto. Cálculo de descuentos que se aplica a productos
+ *
+ * @author Nahuel Ade
+ */
 public class DescuentoProducto {
 
+    /**
+     * Atributos de la clase DescuentoProducto: constantes de descuento que se aplican sobre los productos
+     */
     public static final double DESC_PRODUCTO_NO_CERO = 0.8;
     public static final double DESC_PRODUCTO_OTROS = 0.95;
 
+    /**
+     * Método para aplicar descuento sobre producto dependiendo de la cantidad pedida
+     *
+     * @param precioProducto Precio del producto sobre el que se va a aplicar el descuento
+     * @param numProductos Cantidad de producto
+     */
     public void aplicarDescuento(double precioProducto, int numProductos) {
         double Total;
         precioProducto = precioTotal(numProductos, precioProducto);
@@ -17,9 +31,15 @@ public class DescuentoProducto {
             System.out.println("El total a pagar es:" + Total);
             System.out.println("Enviado");
         }
-
     }
 
+    /**
+     * Método para calcular el precio total del producto dependiendo de la cantidad pedida
+     *
+     * @param numProductos Cantidad de producto
+     * @param precioProducto Precio unitario del producto
+     * @return Precio del producto aplicando un descuento al comprar más de 3 unidades
+     */
     public double precioTotal(int numProductos, double precioProducto) {
         if (numProductos > 3) {
             precioProducto -= 5;
--- src/descuentos/ProductoConDescuento.java
+++ src/descuentos/ProductoConDescuento.java
@@ -1,7 +1,16 @@
 package descuentos;
 
+/**
+ * Clase ProductoConDescuento. Obtención del precio final de la compra
+ *
+ * @author Nahuel Ade
+ */
 public class ProductoConDescuento {
 
+    /**
+     * Método Main. Cálculo del precio final a partir del precio y la cantidad
+     * @param args Argumentos en línea de comandos proporcionados al programa
+     */
     public static void main(String[] args) {
         DescuentoProducto miCarrito = new DescuentoProducto();
 
